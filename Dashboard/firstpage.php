4097eb61be8a0518d8733dd627cf4a39c591c5d4
BryanIY <124484128+BryanIY@users.noreply.github.com>
2024-08-13 15:08:31 +1000

    Add files via upload

---
+++ Dashboard/firstpage.php
@@ -0,0 +1,221 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="Home Page" />
+		<meta name="keywords"	 content="Home, Taxation Accounting" />
+		<meta name="author" 	 content="Bryan Indra Yanto" />
+		<title></title>
+		<link href="https://fonts.googleapis.com/css2?family=Ephesis&family=Lato:wght@100&family=Libre+Baskerville:ital@1&family=Pacifico&display=swap" rel="stylesheet"> 
+		<!-- <link rel="stylesheet" href="style/firstpage.css"/> -->
+	</head>
+	
+	<body class="home">
+		<header class="home">
+			<div class="header">
+				<div><a href="firstpage.php"><img src="image/7.jpeg" alt="Logo" class="logo" /></a></div>
+			</br></br>
+			</div>
+		</header>
+		
+		<div class="section">
+			<figure class="image-section">
+				<img src="image/10.jpeg" alt="image"/>
+			</figure>
+			
+			<div>
+				<p class="paragraph1">Radically transparent database of the world's best remote developers</p>
+				
+				<div class="flex1">
+					<div>For Companies</div>
+					<div>For Job Seekers</div>
+				</div>	
+				
+				<div class="flex2">		
+					<div>Join Sequoia Capital and Y Combinator backed startups
+						 hiring EngineRay developers</div>
+					<div>Join a community of over 15,000 elite developers and get hired in 
+						 top US tech companies</div> 
+				</div>	</br>
+				
+				<div class="flex3">	
+					<div><a href="index.php">Start Hiring!</a></div>
+					<div><a href="https://engineray-dev-userpool.auth.us-east-2.amazoncognito.com/login?client_id=1bshsptan5j6oqhl8t2e3p3i[…]ttp%3A%2F%2Flocalhost%3A8000%2Flogin.html">Login</a></div>	 
+				</div>
+			</div>	</br>	</br>
+		
+			<div class="flex4">
+				<div>15,000+</div>
+				<div>2 Days</div>
+				<div>2 Hours</div>
+			</div>
+			
+			<div class="flex5">
+				<div>Developers</div>
+				<div>To hire a developer</div>
+				<div>of interviewing time of developer</div>
+			</div>
+			
+		</div>
+		
+		</div class="section1">
+			<figure class="image-section1">
+					<img src="image/11.png" alt="image"/>
+			</figure>
+			<p class="paragraph2">©2023 Engineray Inc.</p>
+			
+		</div>
+	<style>
+			* 
+{
+  font-family: Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.logo
+{
+	float: left;
+}
+
+.login
+{
+	color: black;
+	float: right;
+	font-family:'Montserrat';
+	font-size: 30px;
+	border: 2px solid black;
+	border-radius: 5px;
+	padding: 2px;
+	margin-right:50px;
+	text-decoration: none;
+	background-color: #acace6;
+}
+
+/* Section */
+.section
+{
+	width: 100%;
+	height: 120%;
+	background-color: #0693E3;
+}
+
+.section1
+{
+	width: 100%;
+	height: 150%;
+	background-color: white;
+}
+
+/* Images */
+.image-section img
+{
+	float: right;
+	width: 45%;
+}
+.image-section1 img
+{
+	  display: block;
+	  margin-left: auto;
+	  margin-right: auto;
+	  width: 150px;
+}
+
+/*Resize the font size paragraph*/
+.paragraph1
+{
+	font-size: 60px;
+	font-family: 'Montserrat';
+	color: white;
+	padding-left: 5px;
+}
+
+.paragraph2
+{
+	font-size: 20px;
+	font-family: 'Montserrat';
+	color: black;
+	text-align: center;
+}
+
+/*Flex Container*/
+.flex1
+{
+	display: flex;
+}
+.flex1 > div
+{
+	background-color: orange;
+	width: 250px;
+	margin: 10px;
+	text-align: left; 
+	line-height: 40px;
+	padding-left: 10px;
+	font-size: 20px;
+}
+
+.flex2
+{
+	display: flex;
+}
+.flex2 > div
+{
+	width: 250px;
+	margin: 10px;
+	text-align: left; 
+	line-height: 40px;
+	padding-left: 5px;
+	color: white;
+}
+
+.flex3
+{
+	display: flex;
+	gap: 40px;
+}
+.flex3 > div
+{
+	background-color: white;
+	width: 200px;
+	margin-left: 35px;
+	text-align: center; 
+	line-height: 40px;
+	color: #0693E3;
+	border-radius: 5px;
+}
+.flex3 a
+{
+	text-decoration: none;
+}
+
+.flex4, .flex5, .flex6
+{
+	display: flex;
+}
+.flex4 > div
+{
+	color: white;
+	width: 100%;
+	margin: 10px;
+	text-align: center; 
+	line-height: 40px;
+	padding-left: 10px;
+	font-size: 20px;
+}
+
+.flex5 > div
+{
+	color: white;
+	width: 100%;
+	margin: 10px;
+	text-align: center; 
+	line-height: 40px;
+	padding-left: 10px;
+	font-size: 20px;
+}
+
+
+	
+	</style>
+		
+	</body>
+	
+</html>
+
---
+++ Dashboard/index.php
@@ -0,0 +1,151 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="Home Page" />
+		<meta name="keywords"	 content="Home, Taxation Accounting" />
+		<meta name="author" 	 content="Bryan Indra Yanto" />
+		<title>Company' begineer's page</title>
+		<link href="https://fonts.googleapis.com/css2?family=Ephesis&family=Lato:wght@100&family=Libre+Baskerville:ital@1&family=Pacifico&display=swap" rel="stylesheet"> 
+		<link rel="stylesheet" href="style/index.css"/>
+	</head>
+	
+	<body class="home">
+	<header class="home">
+		<div><a href="firstpage.php"><img src="image/7.jpeg" alt="Logo" class="logo" /></a></div>
+	</header>
+	
+	
+	<div class="section1">
+	
+		<figure class="image-section">
+		<img src="image/1.jpeg" alt="image">
+		</figure>
+		<br><br>
+		<p class="paragraph1">Are you looking the newest method to hire engineer developers?</p>
+		
+		<p class="paragraph2">Hire junior or senior remote developers with strong technical
+		and communication skills at an unbeatable prices, ready to work in your timezone!</p></br>
+
+		<div><a href="clogin.php">Login/Signup</a></div>
+	</div>
+	
+	<div class="section2">
+	
+		<figure class="image-section2">
+		<img src="image/2.jpeg" alt="image">
+		</figure>
+	
+	</div>
+	
+	
+	<div class="section3">
+		<p class="paragraph3">Top clients Review Engineray:</p>
+		<p class="paragraph4">Engineray has helped 500+ company's and faster startups hire pre-vetted 
+		engineers on demand. Here's their review:
+		
+		<section class="container">
+			<div class="item">
+				<p class="paragraph5">"Engineray has been providing us with top engineers in Australia. All
+				our other vendors combined don't have the headcount that
+				Engineray does!"</p>
+			</div>
+			<div class="item">
+				<p class="paragraph5">"Engineray has been providing us with top engineers in Australia. All
+				our other vendors combined don't have the headcount that
+				Engineray does!"</p>
+			</div>
+			<div class="item">
+				<p class="paragraph5">"Engineray has been providing us with top engineers in Australia. All
+				our other vendors combined don't have the headcount that
+				Engineray does!"</p>
+			</div>
+		</section>
+	</div>
+	
+	<div class="section4">
+		
+		<figure class="image-section3">
+		<img src="image/3.jpeg" alt="image">
+		</figure>
+		
+	</div>
+	
+	<div class="section5">
+	
+		<figure class="image-section4">
+		<img src="image/4.jpeg" alt="image">
+		</figure>
+	
+		<p class="paragraph6">Watch Problem Solving videos</p>
+		
+		<p class="paragraph7">To find the right engineers, one of the best ways is to watch 
+		how they solve the problems!</p>
+		
+		<div class="container2">
+		
+		<div class="video">
+			<video src="videos/ps1.mp4"controls="controls" width="330">demo</video>
+			<video src="videos/ps1.mp4"controls="controls" width="330">demo</video>
+			<video src="videos/ps1.mp4"controls="controls" width="330">demo</video>
+		</div>
+			
+			
+			
+		</div>
+	</div>
+	
+	<div class="section6">
+	
+		<figure class="image-section6">
+		<img src="image/5.jpeg" alt="image">
+		</figure>
+
+		<p class="paragraph10">Watching Mock interview</p>
+		
+		<p class="paragraph11">Do you know how to effectively interview different candidates?
+		Look at these tips to increase your interview results and pick the right candidates.</p>
+		
+		<div class="container3">
+		
+		<div class="video">
+			<video src="videos/mi1.mp4"controls="controls" width="330">demo</video>
+			<video src="videos/mi1.mp4"controls="controls" width="330">demo</video>
+			<video src="videos/mi1.mp4"controls="controls" width="330">demo</video>
+		</div>
+			
+		</div>
+	</div>
+
+	<div class="section7">
+		<br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<figure class="image-section7">
+		<img src="image/6.jpeg" alt="image">
+		</figure>
+	
+			<div class="section8">
+				<div class="paragraph14">2 Million+ engineers from 150 countries have already joined 
+				Engineray!</div>
+				
+				<div class="flex1">
+					<div> 15+</div>
+					<div>17+</div>
+					<div>7+</div>
+				</div>
+			
+				<div class="flex2">
+					<div>Job Types</div>
+					<div>Technologies</div>
+					<div>Seniority Levels</div>
+				</div>
+				
+			</div>
+			
+	</div>	
+	
+	<div class="section9">
+		<figure class="image-section8">
+		<img src="image/9.png" alt="image">
+		</figure>
+	<div>
+
+</html>
